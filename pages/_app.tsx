29a7339bbe65f1b08991813b1d31f9afef85e941
rmurati <redzep.murati.f@gmail.com>
2021-10-01 15:20:38 +0200

    JAT

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
 export default MyApp;
